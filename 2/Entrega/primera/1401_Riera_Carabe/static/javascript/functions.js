0de34fe04659a8445eb805df73e382535a576206
Juan <superjuan260@hotmail.com>
2018-10-12 20:47:32 +0200

    asfd

---
+++ 2/Entrega/primera/1401_Riera_Carabe/static/javascript/functions.js
@@ -0,0 +1,24 @@
+function updateSearch(){
+  var text=document.getElementById("searchBarBar").value;
+  var lcText = text.toLowerCase()
+  console.log(lcText)
+  var noResultsFlag = 1;
+  var films=document.getElementsByClassName("film")
+  var findPos;
+  for(const film of films){
+    findPos = film.dataset.title.toLowerCase().search(lcText)
+    if(findPos >=0){
+      noResultsFlag = 0;
+      film.style.visibility="visible";
+      document.getElementById("franksearch").style.visibility="hidden";
+    } else {
+      film.style.visibility="hidden";
+    }
+  }
+
+  if(noResultsFlag==1){
+    document.getElementById("franksearch").style.visibility="visible";
+    noResultsFlag=0;
+  }
+  
+}

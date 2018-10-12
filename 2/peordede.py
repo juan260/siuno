04f8530a4ec3c1114e713669e3d333c8506e8850
Juan <superjuan260@hotmail.com>
2018-10-12 22:06:23 +0200

    asdf

--- 2/peordede.py
+++ 2/peordede.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session
 import json
 import os
 import random
 import md5
 app = Flask(__name__)
 
+app.secret_key = 'teamoluis'
 
 # Funcion que devuelve el link que dirige
 # a una pelicula concreta TODO
@@ -12,7 +13,7 @@ def getlink(film):
     return "pelicula.html"
 
 @app.route('/', methods = ['POST', 'GET'])
-def index():
+def index(methods = ['POST', 'GET']):
 	films = json.load(open('data/catalogo.json'))['peliculas']
 	genres = []
 	for film in films:
@@ -26,8 +27,10 @@ def index():
 			if film['genero']==genre:
 				genderedFilms.append(film)
 		films=genderedFilms
-
-	return render_template('index.html', films = films, genres = genres)
+	if('username' in session):
+		return render_template('index.html', films = films, genres = genres, log = session['username'])
+	else:
+		return render_template('index.html', films = films, genres = genres, log = None)
 
 @app.route('/carrito/')
 def carrito():
@@ -35,92 +38,117 @@ def carrito():
     sumPrice = 0
     for film in films:
         sumPrice += film['precio']
-    return render_template('carrito.html', films = films, sumPrice = sumPrice)
-
+	if('username' in session):
+		return render_template('carrito.html', films = films, sumPrice = sumPrice, log = session['username'])
+	else: 
+		return render_template('carrito.html', films = films, sumPrice = sumPrice,log = None)
 @app.route('/contacto/')
 def contacto():
-    return render_template('contacto.html')
+	if('username' in session):
+		return render_template('contacto.html', log = session['username'])
+	else:
+		return render_template('contacto.html', log = None)
 
-@app.route('/iniciosesion/')
-def iniciosesion():
-	username=request.args.get('nombre')
-	if(not(username==None)):
-		root='./data/usuarios/'
-		listaUsuarios = [ item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item)) ]
-		if username in listaUsuarios:
-			ruta = root+username+"/data.json"
-			contrasenia = json.load(open(ruta))['password']
-			print "QUE PASA PENIAA"
-			if contrasenia == md5.new(request.args.get('contrasenia')).hexdigest():
-				return redirect("../")
-			return render_template('iniciosesion.html', usrNoexiste=None, pswEquivocada=1)
-		return render_template('iniciosesion.html', usrNoexiste=1, pswEquivocada=None)
+@app.route('/iniciosesion/', methods = ['POST', 'GET'])
+def iniciosesion(methods = ['POST', 'GET']):
+	
+	if(request.method=='POST'):
+		username=request.form.get('nombre')
+		if(username!=None):
+			root='./data/usuarios/'
+			listaUsuarios = [ item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item)) ]
+			if username in listaUsuarios:
+				ruta = root+username+"/data.json"
+				contrasenia = json.load(open(ruta))['password']
+				print "QUE PASA PENIAA"
+				if contrasenia == md5.new(request.form.get('contrasenia')).hexdigest():
+					session['username']=username
+					return redirect("../")
+				return render_template('iniciosesion.html', usrNoexiste=None, pswEquivocada=1)
+			return render_template('iniciosesion.html', usrNoexiste=1, pswEquivocada=None)
 
-	return render_template('iniciosesion.html', usrNoexiste=None, pswEquivocada=None)
+		return render_template('iniciosesion.html', usrNoexiste=None, pswEquivocada=None)
+	else:
+		return render_template('iniciosesion.html', usrNoexiste=None, pswEquivocada=None)
 
-@app.route('/registro/')
+@app.route('/registro/', methods = ['POST', 'GET'])
 def registro(methods = ['POST', 'GET']):
-	username=request.args.get('usuario')
-	if (not(username==None)):
-		root='./data/usuarios/'
-		listaUsuarios = [ item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item)) ]
-		if username in listaUsuarios:
-			return render_template('registro.html', existe=1)
-		dataR = root+username+"/data.json"
-		historialR = root+username+"/historial.json"
-		os.makedirs(os.path.dirname(dataR))
-		#Creamos historial.json
-		f = open(historialR, "w+")
-		f.write("{\n\t\"peliculas\": []\n}")
-		f = None
+	if(request.method=='POST'):
+		username=request.form.get('usuario')
+		if (username!=None):
+			root='./data/usuarios/'
+			listaUsuarios = [ item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item)) ]
+			if username in listaUsuarios:
+				print("Already existing user")
+				return render_template('registro.html', existe=1)
+			dataR = root+username+"/data.json"
+			historialR = root+username+"/historial.json"
+			os.makedirs(os.path.dirname(dataR))
+			#Creamos historial.json
+			f = open(historialR, "w+")
+			f.write("{\n\t\"peliculas\": []\n}")
+			f = None
+			#Creamos data.json
+			with open(dataR,"w") as f:
+				f.write("{\n\t\"username\": ")
+				f.write("\""+username+"\",\n")
+				
+				f.write("\t\"name\": ")
+				f.write("\""+request.form.get('nombre')+"\",\n")
+				
+				f.write("\t\"surname\": ")
+				f.write("\""+request.form.get('apellidos')+"\",\n")
 
-		#Creamos data.json
-		with open(dataR,"w") as f:
-			f.write("{\n\t\"username\": ")
-			f.write("\""+username+"\",\n")
-			
-			f.write("\t\"name\": ")
-			f.write("\""+request.args.get('nombre')+"\",\n")
-			
-			f.write("\t\"surname\": ")
-			f.write("\""+request.args.get('apellidos')+"\",\n")
+				contraseniaCifrada = md5.new(request.form.get('contrasenia')).hexdigest()
+				f.write("\t\"password\": ")
+				f.write("\""+contraseniaCifrada+"\",\n")
+				
+				f.write("\t\"email\": ")
+				f.write("\""+request.form.get('correo')+"\",\n")
+				
+				f.write("\t\"creditcard\": ")
+				f.write("\""+request.form.get('tarjeta')+"\",\n")
+				
+				f.write("\t\"secretno\": ")
+				f.write("\""+request.form.get('secretnum')+"\",\n")
+				
+				f.write("\t\"saldo\": ")
+				f.write(str(random.randint(1,100))+"\n}")
+				session['username']=username
+				print("SI ERA POST4")
+				return redirect("../")
+				#return redirect("../")
 
-			contraseniaCifrada = md5.new(request.args.get('contrasenia')).hexdigest()
-			f.write("\t\"password\": ")
-			f.write("\""+contraseniaCifrada+"\",\n")
-			
-			f.write("\t\"email\": ")
-			f.write("\""+request.args.get('correo')+"\",\n")
-			
-			f.write("\t\"creditcard\": ")
-			f.write("\""+request.args.get('tarjeta')+"\",\n")
-			
-			f.write("\t\"secretno\": ")
-			f.write("\""+request.args.get('secretnum')+"\",\n")
-			
-			f.write("\t\"saldo\": ")
-			f.write(str(random.randint(1,100))+"\n}")
-			return redirect("../")
+		
+		return render_template('registro.html', existe=None)
 
-
-	return render_template('registro.html', existe=None)
+	else:
+		return render_template('registro.html', existe=None)
 
 @app.route('/cuenta/')
 def cuenta():
-    return render_template('cuenta.html')
+	if('username' in session):
+		return render_template('cuenta.html',log = session['username'])
+	else:
+		return render_template('cuenta.html',log = None)
 
 @app.route('/finalizarCompra/')
 def finalizarCompra():
-    films = json.load(open('data/catalogo.json'))['peliculas']
-    sumPrice = 0
-    for film in films:
-        sumPrice += film['precio']
-    return render_template('finalizarCompra.html', films = films, sumPrice = sumPrice)
-
+	films = json.load(open('data/catalogo.json'))['peliculas']
+	sumPrice = 0
+	for film in films:
+		sumPrice += film['precio']
+	if('username' in session):
+		return render_template('finalizarCompra.html', films = films, sumPrice = sumPrice, log = session['username'])
+	else: 
+		return render_template('finalizarCompra.html', films = films, sumPrice = sumPrice, log = None)
 @app.route('/historialCompras/')
 def historialCompras():
 	films = json.load(open('data/catalogo.json'))['peliculas']
-	return render_template('historialCompras.html', films = films)
+	if('username' in session):
+		return render_template('historialCompras.html', films = films, log = session['username'])
+	else:
+		return render_template('historialCompras.html', films = films, log = None)
 
 @app.route('/pelicula/<path:name>')
 def pelicula(name):
@@ -128,9 +156,18 @@ def pelicula(name):
 	for film in films:
 		print(name + str(film['id']))
 		if int(name) == film['id']:
-			return render_template('pelicula.html', film = film)
-
-	return render_template('pelicula.html', film = None)
+			if('username' in session):
+				return render_template('pelicula.html', film = film, log = session['username'])
+			else:
+				return render_template('pelicula.html', film = film, log = None)
+	if('username' in session):
+		return render_template('pelicula.html', film = None, log = session['username'])
+	else:
+		return render_template('pelicula.html', film = None, log = None)
+@app.route('/logout/')
+def logout():
+	session.pop('username', None)
+	return redirect("../")
 
 
 if __name__ == '__main__':

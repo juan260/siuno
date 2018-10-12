2c4f374a6dac1812f04b2945c439d1850890f2da
luiscarabe <luiscarabe@gmail.com>
2018-10-12 20:07:06 +0200

    login y registro done

--- 2/peordede.py
+++ 2/peordede.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import json
+import os
+import random
+import md5
 app = Flask(__name__)
 
 
@@ -8,7 +11,7 @@ app = Flask(__name__)
 def getlink(film):
     return "pelicula.html"
 
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def index():
 	films = json.load(open('data/catalogo.json'))['peliculas']
 	genres = []
@@ -40,11 +43,67 @@ def contacto():
 
 @app.route('/iniciosesion/')
 def iniciosesion():
-    return render_template('iniciosesion.html')
+	username=request.args.get('nombre')
+	if(not(username==None)):
+		root='./data/usuarios/'
+		listaUsuarios = [ item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item)) ]
+		if username in listaUsuarios:
+			ruta = root+username+"/data.json"
+			contrasenia = json.load(open(ruta))['password']
+			print "QUE PASA PENIAA"
+			if contrasenia == md5.new(request.args.get('contrasenia')).hexdigest():
+				return redirect("../")
+			return render_template('iniciosesion.html', usrNoexiste=None, pswEquivocada=1)
+		return render_template('iniciosesion.html', usrNoexiste=1, pswEquivocada=None)
+
+	return render_template('iniciosesion.html', usrNoexiste=None, pswEquivocada=None)
 
 @app.route('/registro/')
-def registro():
-    return render_template('registro.html')
+def registro(methods = ['POST', 'GET']):
+	username=request.args.get('usuario')
+	if (not(username==None)):
+		root='./data/usuarios/'
+		listaUsuarios = [ item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item)) ]
+		if username in listaUsuarios:
+			return render_template('registro.html', existe=1)
+		dataR = root+username+"/data.json"
+		historialR = root+username+"/historial.json"
+		os.makedirs(os.path.dirname(dataR))
+		#Creamos historial.json
+		f = open(historialR, "w+")
+		f.write("{\n\t\"peliculas\": []\n}")
+		f = None
+
+		#Creamos data.json
+		with open(dataR,"w") as f:
+			f.write("{\n\t\"username\": ")
+			f.write("\""+username+"\",\n")
+			
+			f.write("\t\"name\": ")
+			f.write("\""+request.args.get('nombre')+"\",\n")
+			
+			f.write("\t\"surname\": ")
+			f.write("\""+request.args.get('apellidos')+"\",\n")
+
+			contraseniaCifrada = md5.new(request.args.get('contrasenia')).hexdigest()
+			f.write("\t\"password\": ")
+			f.write("\""+contraseniaCifrada+"\",\n")
+			
+			f.write("\t\"email\": ")
+			f.write("\""+request.args.get('correo')+"\",\n")
+			
+			f.write("\t\"creditcard\": ")
+			f.write("\""+request.args.get('tarjeta')+"\",\n")
+			
+			f.write("\t\"secretno\": ")
+			f.write("\""+request.args.get('secretnum')+"\",\n")
+			
+			f.write("\t\"saldo\": ")
+			f.write(str(random.randint(1,100))+"\n}")
+			return redirect("../")
+
+
+	return render_template('registro.html', existe=None)
 
 @app.route('/cuenta/')
 def cuenta():

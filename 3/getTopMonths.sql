621cc7f7f29888c1d9efa4d850628d0df9d3d607
juan260 <superjuan260@hotmail.com>
2018-11-14 00:14:57 +0100

    vamo luiss

---
+++ 3/getTopMonths.sql
@@ -0,0 +1,34 @@
+﻿
+
+CREATE OR REPLACE FUNCTION getTopMonths (integer, integer) RETURNS table(
+	date_part1 double precision,
+	movietitle1 VARCHAR(255),
+	sum1 BIGINT
+
+)
+	
+	as $$
+	declare
+	BEGIN	
+		
+		return query select year1, month1, sum(quantity) as quant, sum(money) as price
+                	from (select extract(YEAR from orderdate) as year1, extract(MONTH from orderdate) as month1, 
+					movieid, quantity
+                        	from (select ORD.orderdate, quantity, price as money
+                                	from orders as ORD,
+                                        	orderdetail as ORDET
+	                                where ORD.orderid = ORDET.orderid)
+        	                		as YearMovIdAndQuantity,
+                                	group by year1, month1
+					where quant>$1 or price>$2;
+
+	
+	END;
+	$$ LANGUAGE 'plpgsql';
+
+
+select getTopMonths(1, 2);
+
+DROP FUNCTION gettopmonths(integer, integer);
+
+

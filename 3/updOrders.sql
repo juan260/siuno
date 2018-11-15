b1d5d8a5d59aae470911855f7b7a0711a24dda49
Juan Riera Gomez <superjuan260@hotmail.com>
2018-11-15 18:31:51 +0100

    asdf

--- 3/updOrders.sql
+++ 3/updOrders.sql
@@ -3,30 +3,55 @@
 -- y busca si el usuario tiene otros pedidos en carrito 
 -- y los elimina
 -- INNECESARIA
-CREATE OR REPLACE FUNCTION checkOrders (integer, integer) RETURNS void
-	as $$
-	declare
-	BEGIN
-        DELETE FROM orders 
-            where orders.customerid=$1 and
-                orders.orderid <> $2 and
-                orders.status = NULL
+--CREATE OR REPLACE FUNCTION checkOrders (integer, integer) RETURNS void
+--	as $$
+--	declare
+--	BEGIN
+  --      DELETE FROM orders 
+    --        where orders.customerid=$1 and
+      --          orders.orderid <> $2 and
+       --         orders.status = NULL
+--
+  --  END;    
+   -- $$ LANGUAGE 'plpgsql';
 
-    END;    
-    $$ LANGUAGE 'plpgsql';
 
-CREATE OR REPLACE FUNCTION isCarrito (id integer)
+CREATE OR REPLACE FUNCTION updAddOrders () RETURNS TRIGGER
     as $$
     BEGIN
         
-
+	update
+		orders as ORD
+	set
+		netamount=(ORD.netamount+NEW.price)
+	where ORD.orderid=new.orderid and ORD.status=NULL;
     END;
     $$ LANGUAGE 'plpgsql';
 
 
-CREATE OR REPLACE TRIGGER updOrders
-    BEFORE UPDATE ON orderdetail
+CREATE TRIGGER addOrdersTrig
+    BEFORE INSERT ON orderdetail
     FOR EACH ROW
-    WHEN isCarrito(NEW.orderid)
-    EXECUTE PROCEDURE checkOrders(NEW.customerid, NEW.orderid);
+    --EXECUTE PROCEDURE checkOrders(NEW.customerid, NEW.orderid);
+    EXECUTE PROCEDURE updAddOrders();
+
+CREATE OR REPLACE FUNCTION updSubtractOrders () RETURNS TRIGGER
+    as $$
+    BEGIN
+        
+	update
+		orders as ORD
+	set
+		netamount=ORD.netamount-OLD.price
+	where ORD.orderid=OLD.orderid and ORD.status=NULL;
+    END;
+    $$ LANGUAGE 'plpgsql';
+
+
+CREATE TRIGGER subOrdersTrig
+    BEFORE INSERT ON orderdetail
+    FOR EACH ROW
+    --EXECUTE PROCEDURE checkOrders(NEW.customerid, NEW.orderid);
+    EXECUTE PROCEDURE updSubtractOrders();
+
 

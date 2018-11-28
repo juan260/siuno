6e467ae73895548aafbf8d1d38a96aa8467b7f43
juan260 <superjuan260@hotmail.com>
2018-11-28 11:51:01 +0100

    adf

--- 3/getTopMonths.sql
+++ 3/getTopMonths.sql
@@ -1,5 +1,9 @@
 ﻿
-
+-- Funcion que dados dos enteros, una cantidad primero
+-- y una cantidad de dinero como segundo argumento,
+-- devuelve el mes de los anios en los que se supero
+-- o la cuantia del primer argumento o la cuantia economica
+-- del segundo argumento.
 CREATE OR REPLACE FUNCTION getTopMonths (integer, integer) RETURNS table(
 	year1 double precision,
 	month1 text,
@@ -12,13 +16,17 @@ CREATE OR REPLACE FUNCTION getTopMonths (integer, integer) RETURNS table(
 	declare
 	BEGIN	
 		
-		return query select * from (select extract(YEAR from orderdate) as year, 
+		return query select * from 
+			-- Las ganancias economicas y las cantidades de ventas
+			-- agrupados por meses y anios
+			(select extract(YEAR from orderdate) as year, 
 				      to_char(orderdate, 'Month') as month,  
 					sum(ORDET.quantity) as quantity, sum(price) as money
                                 	from orders as ORD,
                                         	orderdetail as ORDET
 	                                where ORD.orderid = ORDET.orderid 
 					group by year, month) as OrdersWithPrizesAndQuantities
+					-- Hacemos las comprobaciones
 					where OrdersWithPrizesAndQuantities.money > $2 or 
 						OrdersWithPrizesAndQuantities.quantity > $1;
 
